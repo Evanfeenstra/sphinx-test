f0e36d31d184067422b304e716755ad2b591de9f
Evanfeenstra <evanfeenstra@gmail.com>
2021-01-26 10:37:17 -0800

    asdf

--- test.js
+++ test.js
@@ -2,9 +2,16 @@ var test = require('ava');
 var http = require('ava-http');
 
 var nodes = require('./nodes.json')
+var node1 = nodes[0]
+
+function makeArgs(node) {
+  return {
+    headers : {'x-user-token':node.authToken}
+  }
+}
 
 test('test i am contact #1', async t => {
-  const res = await http.get('https://xaalwgb234n7gy2.039.sphinxnodes.chat/contacts', {headers:{'x-user-token':nodes[0].authToken}});
+  const res = await http.get(node1.ip+'/contacts', makeArgs(node1));
   console.log(res)
 	t.true(typeof res === 'object'); // json object by default
   t.deepEqual(res, {expected: 'output'}); // deepEqual comparison
